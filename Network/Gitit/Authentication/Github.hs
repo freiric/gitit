5ee5ccb5f37c610a1e17f0e5170d87f827dff811
Freiric Barral <freiric@gmail.com>
2014-09-21 10:36:31 +0200

    use http-conduit instead of http-client in github authentication, reject github callback whose state differ from the one we previously sent (to protect against cross-site request forgery attacks)

--- Network/Gitit/Authentication/Github.hs
+++ Network/Gitit/Authentication/Github.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
-module Network.Gitit.Authentication.Github (loginGithubUser, getGithubUser, GithubCallbackPars) where
+module Network.Gitit.Authentication.Github ( loginGithubUser
+                                           , getGithubUser
+                                           , GithubCallbackPars) where
 
 import Network.OAuth.OAuth2
 
@@ -9,54 +11,70 @@ import Network.Gitit.Server
 import Network.Gitit.State
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy as BSL
-import Network.HTTP.Client
+import Network.HTTP.Conduit
 import Network.HTTP.Client.TLS
 import Control.Monad (liftM, mplus, mzero)
 import Data.Aeson
 import Data.Text (Text, pack, unpack)
 import Data.Text.Encoding (encodeUtf8)
 import Control.Applicative
+import Control.Monad.Trans (liftIO)
 import Data.Char (chr)
+import Data.UUID (toString)
+import Data.UUID.V4 (nextRandom)
 
 loginGithubUser :: OAuth2 -> Handler
 loginGithubUser githubKey = do
-  let state = "testGithubApi"
+  state <- liftIO $ fmap toString nextRandom
+  key <- newSession (sessionDataGithubState state)
+  cfg <- getConfig
+  addCookie (MaxAge $ sessionTimeout cfg) (mkCookie "sid" (show key))
   let scopes = "user:email"
-  let url = authorizationUrl githubKey `appendQueryParam` [("state", state), ("scope", scopes)]
+  let url = authorizationUrl githubKey `appendQueryParam` [("state", BS.pack state), ("scope", scopes)]
   seeOther (BS.unpack url) $ toResponse ("redirecting to github" :: String)
 
 getGithubUser :: OAuth2                  -- ^ Oauth2 configuration (client secret)
               -> GithubCallbackPars      -- ^ Authentication code gained after authorization
-              -> IO (Either String User) -- ^ user email and name (password 'none')
-getGithubUser githubKey githubCallbackPars = do
-  let (Just code) = rCode githubCallbackPars
-  let setting = tlsManagerSettings
-  mgr <- newManager setting
-  token <- fetchAccessToken mgr githubKey (sToBS code)
-  let mUser = case token of
-                Right at -> do
-                      uinfo <- userInfo mgr at
-                      minfo <- mailInfo mgr at
-                      case (uinfo, minfo) of
-                         (Right githubUser, Right githubUserMail) -> do
-                                  user <- mkUser (unpack $ gname githubUser)
-                                                 (unpack $ email $ head githubUserMail)
-                                                 "none"
-                                  return $ Right user
-                         (Left err, _) -> return $ Left $ lbsToStr err
-                         (_, Left err) -> return $ Left $ lbsToStr err
-                Left err ->
-                     return $ Left $  "no access token found yet: " ++ lbsToStr  err
-  closeManager mgr
-  mUser
+              -> String                  -- ^ Github state, we expect the state we sent in loginGithubUser
+              -> GititServerPart (Either String User) -- ^ user email and name (password 'none')
+getGithubUser githubKey githubCallbackPars githubState =
+  withManagerSettings tlsManagerSettings getUserInternal
+  where
+    getUserInternal mgr = liftIO $ do
+      let (Just state) = rState githubCallbackPars
+      if state == githubState
+        then do
+          let (Just code) = rCode githubCallbackPars
+          token <- fetchAccessToken mgr githubKey (sToBS code)
+          let mUser = case token of
+                             Right at -> do
+                                        uinfo <- userInfo mgr at
+                                        minfo <- mailInfo mgr at
+                                        case (uinfo, minfo) of
+                                          (Right githubUser, Right githubUserMail) -> do
+                                                              user <- mkUser (unpack $ gname githubUser)
+                                                                      (unpack $ email $ head githubUserMail)
+                                                                      "none"
+                                                              return $ Right user
+                                          (Left err, _) -> return $ Left $ lbsToStr err
+                                          (_, Left err) -> return $ Left $ lbsToStr err
+                             Left err ->
+                                 return $ Left $  "no access token found yet: " ++ lbsToStr  err
+          mUser
+        else
+          return $ Left $ "returned state: " ++ state ++ ", expected state: " ++  githubState
 
-data GithubCallbackPars = GithubCallbackPars { rCode :: Maybe String }
+
+
+data GithubCallbackPars = GithubCallbackPars { rCode :: Maybe String
+                                             , rState :: Maybe String }
                           deriving Show
 
 instance FromData GithubCallbackPars where
     fromData = do
-         vcode <- liftM Just (look "code") `mplus` return Nothing
-         return GithubCallbackPars {rCode = vcode }
+         vCode <- liftM Just (look "code") `mplus` return Nothing
+         vState <- liftM Just (look "state") `mplus` return Nothing
+         return GithubCallbackPars {rCode = vCode, rState = vState}
 
 userInfo :: Manager -> AccessToken -> IO (OAuth2Result GithubUser)
 userInfo mgr token = authGetJSON mgr token "https://api.github.com/user"
